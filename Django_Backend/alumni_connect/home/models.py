c96cd80454792dcf787b734760dc4f731692cfc8
Sanidhya S. Johri <johri.2@iitj.ac.in>
2022-11-12 16:57:56 +0530

    almighty push the second

--- Django_Backend/alumni_connect/home/models.py
+++ Django_Backend/alumni_connect/home/models.py
@@ -12,9 +12,19 @@ class custom_user(models.Model):
     email = models.CharField(max_length=100, null= False)
     phone = models.CharField(max_length=13, null = True)
     branch = models.CharField(max_length=50, null=False)
+    isAlumni = models.BooleanField() #if 1 then alumni, if not , then i dont know
     year_joined = models.DateField()
-    year_passed = models.DateField()
+    year_passed = models.DateField(default=True)
     password = models.CharField(max_length=50, null = False)
     connections = ArrayField(models.CharField(max_length=100), default=list)
+    company = models.CharField(max_length=100, null= True)
+    working_experience = models.IntegerField()
+    working_field = models.CharField(max_length=50)
 
+
+    
+
+
+
+    
 
