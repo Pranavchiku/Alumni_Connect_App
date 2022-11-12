bcf0892fa1bc4020c614350b1217a2b6994299fa
Sanidhya S. Johri <johri.2@iitj.ac.in>
2022-11-12 15:27:29 +0530

    push1

--- Django_Backend/alumni_connect/home/models.py
+++ Django_Backend/alumni_connect/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class custom_user(models.Model):
+    name  = models.CharField(max_length= 100, null=False)
+    email = models.CharField(max_length=100, null= False)
+    phone = models.CharField(max_length=13, null = True)
+    branch = models.CharField(max_length=50, null=False)
+    year_joined = models.DateField()
+    year_passed = models.DateField()
+    password = models.CharField(max_length=50, null = False)
+    connections = models.ManyToManyField(User)    
+
