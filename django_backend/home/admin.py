1d1f683914dc039e93a197350b4c7cc4a1d90675
Pranavchiku <goswami.4@iitj.ac.in>
2022-11-26 19:25:44 +0530

    deleting the previous models

--- django_backend/home/admin.py
+++ django_backend/home/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(customuser)
-admin.site.register(post)
 admin.site.register(Userinfo)
 admin.site.register(Educationinfo)
 admin.site.register(Workinfo)
--- django_backend/home/models.py
+++ django_backend/home/models.py
@@ -3,36 +3,6 @@ from django.contrib.postgres.fields import ArrayField
 
 
 # Create your models here.
-
-class customuser(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100, default=None)
-    phone = models.CharField(max_length=13, default=None)
-    branch = models.CharField(max_length=50, default=None)
-    isAlumni = models.BooleanField(default=1) #if 1 then alumni, if not , then i dont know
-
-    #fields for students/alumni
-    SDE = models.BooleanField(default=False)
-    ML_DL = models.BooleanField(default=False)
-    DS = models.BooleanField(default=False)
-    Management = models.BooleanField(default=False)
-    Others = models.BooleanField(default=False)
-
-    year_joined = models.DateField(default=None)
-    year_passed = models.DateField(default= None)
-    password = models.CharField(max_length=50 ,default = None)
-    working_experience = models.IntegerField(default = None)
-    company = models.CharField(max_length=100 ,default = None)
-    # connections = ArrayField(models.CharField(max_length=100, default=None), default=None)
-    connections = models.ManyToManyField('self')
-
-class post(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100, default=None)
-    date = models.DateField( default=None)
-    image = models.CharField(max_length=100, default=None)
-    descript = models.CharField(max_length=500,  default=None)
-
 class Userinfo(models.Model):
     email = models.CharField(max_length=100, unique = True)
     name = models.CharField(max_length=100, default = None)
@@ -68,4 +38,3 @@ class Userpost(models.Model):
     date = models.DateField(default=None)
     image = models.CharField(max_length=100, default=None)
     descript = models.CharField(max_length=500, default=None)
-    
