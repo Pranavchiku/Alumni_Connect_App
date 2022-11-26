46a7303f3a5b6f89e3e393ca9d336b65bc570bfb
Sanidhya S. Johri <johri.2@iitj.ac.in>
2022-11-26 17:44:41 +0530

    almighty push

--- backend/home/models.py
+++ backend/home/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
 class custom_user(models.Model):
-    name  = models.CharField(max_length= 100, null=False)
-    profile= models.CharField(max_length= 100, null=True)
-    email = models.CharField(max_length=100, null= False)
-    phone = models.CharField(max_length=13, null = True)
-    branch = models.CharField(max_length=50, null=False)
+    name  = models.CharField(max_length= 100)
+    profile= models.CharField(max_length= 100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13)
+    branch = models.CharField(max_length=50)
     isAlumni = models.BooleanField(default=1) #if 1 then alumni, if not , then i dont know
     year_joined = models.DateField()
     year_passed = models.DateField(default=True)
-    password = models.CharField(max_length=50, null = False)
+    password = models.CharField(max_length=50)
     connections = ArrayField(models.CharField(max_length=100), default=list)
-    company = models.CharField(max_length=100, null= True)
+    company = models.CharField(max_length=100)
     working_experience = models.IntegerField()
     
     #fields for students/alumni
---
+++ django_backend/home/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+# Create your models here.
+
+class customuser(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, default=None)
+    phone = models.CharField(max_length=13, default=None)
+    branch = models.CharField(max_length=50, default=None)
+    isAlumni = models.BooleanField(default=1) #if 1 then alumni, if not , then i dont know
+
+    #fields for students/alumni
+    SDE = models.BooleanField(default=False)
+    ML_DL = models.BooleanField(default=False)
+    DS = models.BooleanField(default=False)
+    Management = models.BooleanField(default=False)
+    Others = models.BooleanField(default=False)
+
+    year_joined = models.DateField(default=None)
+    year_passed = models.DateField(default= None)
+    password = models.CharField(max_length=50 ,default = None)
+    working_experience = models.IntegerField(default = None)
+    company = models.CharField(max_length=100 ,default = None)
+    # connections = ArrayField(models.CharField(max_length=100, default=None), default=None)
