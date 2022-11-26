5acff89254c4efcfd371cf18da6b966a10558bd6
HarshitaKalani <95532771+HarshitaKalani@users.noreply.github.com>
2022-11-26 16:26:36 +0530

    pushadd

--- Django_Backend/alumni_connect/home/admin.py
+++ Django_Backend/alumni_connect/home/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-
-admin.site.register(custom_user)
-admin.site.register(post)
--- Django_Backend/alumni_connect/home/models.py
+++ Django_Backend/alumni_connect/home/models.py
@@ -2,40 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
-class user(models.Model):
-    name = models.CharField(max_length=100)
 
-# Create your models here.
-
-class custom_user(models.Model):
-    name  = models.CharField(max_length= 100, null=False)
-    # profile= models.CharField(max_length= 100, null=True)
-    email = models.CharField(max_length=100, null= False)
-    phone = models.CharField(max_length=13, null = True)
-    branch = models.CharField(max_length=50, null=False)
-    # isAlumni = models.BooleanField() #if 1 then alumni, if not , then i dont know
-    year_joined = models.DateField()
-    year_passed = models.DateField(default=True)
-    password = models.CharField(max_length=50, null = False)
-    connections = ArrayField(models.CharField(max_length=100), default=list)
-    company = models.CharField(max_length=100, null= True)
-    working_experience = models.IntegerField()
-    
-    #fields for students/alumni
-    SDE = models.BooleanField(default=False)
-    ML_DL = models.BooleanField(default=False)
-    DS = models.BooleanField(default=False)
-    Management = models.BooleanField(default=False)
-    Others = models.BooleanField(default=False)
-
-
-class post(models.Model):
-    name = models.CharField(max_length=100, null=False)
-    email = models.CharField(max_length=100, null= False)
-    date = models.DateField()
-    image = models.CharField(max_length=100, null=True)
-    descript = models.CharField(max_length=500)
-    
 
 
 
