86af4730278b06357aeb043784b05668890be17f
Pranavchiku <goswami.4@iitj.ac.in>
2022-11-27 14:56:26 +0530

    succesfully integrated work info api with flutter

--- Flutter_Frontend/alumni_connect_app/lib/pages/work_field.dart
+++ Flutter_Frontend/alumni_connect_app/lib/pages/work_field.dart
@@ -1,23 +1,19 @@
-import 'package:alumni_connect_app/pages/work_field.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'index.dart';
 
-class EducationInfo extends StatefulWidget {
+class WorkInfo extends StatefulWidget {
   String email;
-  bool isAlum;
-
-  EducationInfo({required this.email, required this.isAlum});
+  WorkInfo({required this.email});
 
   @override
-  State<EducationInfo> createState() => _EducationInfoState();
+  State<WorkInfo> createState() => _WorkInfoState();
 }
 
-class _EducationInfoState extends State<EducationInfo> {
+class _WorkInfoState extends State<WorkInfo> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String? _degree, _yearJoined, _yearGraduated, _branch;
+  String? _company, _workExp;
 
   signup() async {
     if (_formKey != null &&
@@ -27,33 +23,20 @@ class _EducationInfoState extends State<EducationInfo> {
 
       Map<String, dynamic> requestPayload = {
         "email": widget.email,
-        "degree": _degree,
-        "year_joined": _yearJoined,
-        "year_passed": _yearGraduated,
-        "branch": _branch
+        "company": _company,
+        "work": int.parse(_workExp!)
       };
 
-      var url = Uri.parse('http://127.0.0.1:8001/api/user_education_info/');
+      var url = Uri.parse('http://127.0.0.1:8001/api/user_work_info/');
       var response = await http.post(
         url,
         body: jsonEncode(requestPayload),
       );
-
       var body = jsonDecode(response.body);
 
       if (response.statusCode == 201) {
-        if (widget.isAlum) {
-          Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => WorkInfo(email: widget.email)),
-              (route) => false);
-        } else {
-          Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (context) => Index()),
-              (route) => false);
-        }
+        Navigator.pushAndRemoveUntil(context,
+            MaterialPageRoute(builder: (context) => Index()), (route) => false);
       } else {
         showError("Internal Server Error");
       }
@@ -115,7 +98,7 @@ class _EducationInfoState extends State<EducationInfo> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text(
-                "Education Information",
+                "Work Information",
                 style: Theme.of(context).textTheme.headline1!.copyWith(
                       fontSize: size.width * 0.1,
                       fontWeight: FontWeight.bold,
@@ -135,14 +118,14 @@ class _EducationInfoState extends State<EducationInfo> {
                       child: TextFormField(
                         validator: (input) {
                           if (input != null && input.isEmpty)
-                            return "Enter Degree";
+                            return "Enter Company name";
                         },
                         decoration: InputDecoration(
                           border: InputBorder.none,
-                          hintText: "Degree",
+                          hintText: "Company Name",
                           prefixIcon: Icon(Icons.cast_for_education),
                         ),
-                        onSaved: (input) => _degree = input!,
+                        onSaved: (input) => _company = input!,
                       ),
                     ),
                     SizedBox(
@@ -157,58 +140,14 @@ class _EducationInfoState extends State<EducationInfo> {
                       child: TextFormField(
                         validator: (input) {
                           if (input != null && input.isEmpty)
-                            return "Enter Branch";
+                            return "Enter experience";
                         },
                         decoration: InputDecoration(
                           border: InputBorder.none,
-                          hintText: "Branch",
+                          hintText: "Experience",
                           prefixIcon: Icon(Icons.featured_play_list_outlined),
                         ),
-                        onSaved: (input) => _branch = input!,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                      child: TextFormField(
-                        validator: (input) {
-                          if (input != null && input.isEmpty)
-                            return "Enter year of joining";
-                        },
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Year of Joining",
-                          prefixIcon: Icon(Icons.arrow_right_alt_sharp),
-                        ),
-                        onSaved: (input) => _yearJoined = input!,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                      child: TextFormField(
-                        validator: (input) {
-                          if (input != null && input.isEmpty)
-                            return "Enter year of graduation";
-                        },
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Year of graduation",
-                          prefixIcon: Icon(Icons.grade_outlined),
-                        ),
-                        onSaved: (input) => _yearGraduated = input!,
+                        onSaved: (input) => _workExp = input!,
                       ),
                     ),
                   ],
@@ -225,7 +164,7 @@ class _EducationInfoState extends State<EducationInfo> {
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: TextButton(
-                  onPressed: signup,
+                  onPressed: () {},
                   child: Text(
                     "Next",
                     style: TextStyle(
